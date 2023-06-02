25bc61256bce83e5b9a64410784be919e5c62c61
Real G <namanh8t@gmail.com>
2023-06-02 12:51:06 +0700

    12h50 2/5

--- E-cardvisit-main/src/App.jsx
+++ E-cardvisit-main/src/App.jsx
@@ -79,62 +79,57 @@ const App = () => {
   };
   return (
     <div className="container">
-      <div className="row">
-        <div className="mx-auto">
-          <div className="mx-auto text-center">
-            <img src="./img/71e42070-d8ef-488e-b916-276e56336e003.jpg" alt="Avatar" className="rounded-circle m-4 mx-auto text-center" />
-            <span className="name text-center">
-              <h1>{user.name}</h1>
-            </span>
-            <span className="description text-center">
-              <p>
-                {user.title}<br></br>
-                {user.organization}
-              </p>
-            </span>
-          </div>
+      <div className="cover-photo"></div>
+      <div className="row-1">
+        <div className="d-flex">
+          <img
+            src="./img/71e42070-d8ef-488e-b916-276e56336e003.jpg"
+            alt="Avatar"
+            className="avatar rounded-circle "
+          />
+          <div className='container infor-box row '>
+            <div className="col">
+              <div className="name description text-center">
+                <h1>{user.name}</h1>
+                <p>
+                  {user.title}
+                  <br />
+                  {user.organization}
+                </p>
+              </div></div>
+            <div className="col ">
 
-          <button className="btn btn-download  btn-outline-info m-4 d-flex w-60 mx-auto">
-            <a href="./contact/test.vcf" download>
-              <span className><i className="bi bi-download " />
-                <span className="ms-3"> Lưu thông tin</span>
-              </span>
-            </a>
-          </button>
-
-          <div className="fast-icon mx-auto text-center fa-xs">
-            {fast_icon}
-          </div>
-
-          <div className="social_media-box">
-
-            <div className="social_media-bg mx-auto pt-2 pb-4 rounded-top border">
-
-              <button className="btn btn-outline-success m-2 d-flex w-75 mx-auto">
-                <i className="bi bi-phone " />
-                <span className="ms-3">Phone</span>
+              <button className="btn btn-download  btn-outline-info d-flex w-60 mx-auto">
+                <a href="./contact/test.vcf" download>
+                  <span className><i className="bi bi-arrow-down-up " />
+                    <span className="ms-3"> Thêm vào danh bạ</span>
+                  </span>
+                </a>
               </button>
-              {phoneNumber && (
-                <p className="phone-number text-center">{phoneNumber}</p>
-              )}
-              {/* <button className="btn btn-outline-success m-2 d-flex w-75 mx-auto">
-                <span className="btn-icon "><i className="bi bi-envelope " /></span>
-                <span className="ms-3">Email</span>
-              </button>
-              <button className="btn btn-outline-success m-2 d-flex w-75 mx-auto">
-                <span className="btn-icon "><i className="bi bi-linkedin " /></span>
-                <span className="ms-3">Linked-in</span>
-              </button>
-              <button className="btn btn-outline-success m-2 d-flex w-75 mx-auto">
-                <span className="btn-icon "><i className="bi bi-facebook " /></span>
-                <span className="ms-3">Social Media</span>
-              </button> */}
-              {link_button}
             </div>
+          </div></div>
+
+        <div className="fast-icon mx-auto text-center fa-xs box mt-4">
+          {fast_icon}
+        </div>
+        <div className="social_media-box">
+
+          <div className="social_media-bg mx-auto pt-2 pb-4 rounded-top border">
+
+            <button className="btn btn-outline-success m-2 d-flex w-75 mx-auto">
+              <i className="bi bi-phone " />
+              <span className="ms-3">Phone</span>
+            </button>
+            {phoneNumber && (
+              <p className="phone-number text-center">{phoneNumber}</p>
+            )}
+
+            {link_button}
           </div>
         </div>
       </div>
     </div>
+
   );
 };
 
